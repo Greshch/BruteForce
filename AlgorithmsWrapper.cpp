bcfd2efb4ec1a387d2fe3407ecbc2e5473e0bd29
Greshch <greshchook@gmail.com>
2022-11-06 13:04:05 +0200

    Add class AlgorithmsWapper

---
+++ AlgorithmsWrapper.cpp
@@ -0,0 +1,12 @@
+#include "AlgorithmsWrapper.h"
+
+#include <string>
+#include <vector>
+#include <fstream>
+#include <exception>
+#include <iostream>
+#include <algorithm>
+
+#include "openssl/evp.h"
+#include <openssl/aes.h>
+#include "openssl/sha.h"
