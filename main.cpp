5c55bdf19d879940db29259e292df5e0d73abb64
Greshch <greshchook@gmail.com>
2022-11-06 20:16:41 +0200

    task 1 was finished and some mess was hiden

--- main.cpp
+++ main.cpp
@@ -11,14 +11,12 @@ int main()
 {
     std::string pass = "pass";
     std::string foldersPath = "D:/projects/cplus/Apriorit/second/BruteForce/Debug/";
-    //std::string nameSimpleText = foldersPath + "plain_text";
-    std::string nameEncryptedText = foldersPath + "chipher_text" /*"encrypted_text"*/;
+    std::string nameEncryptedText = foldersPath + "chipher_text";
     std::string nameDecryptedText = foldersPath + "decrypted_text";
     try
     {
         std::unique_ptr<AlgorithmsBase> algo(new Md_5Algorithm);
         algo.get()->PasswordToKey(pass);
-        //algo.get()->Encrypt(nameEncryptedText, nameSimpleText);
         algo.get()->Decrypt(nameDecryptedText, nameEncryptedText);
     }
     catch (const std::runtime_error& ex)
