4e77714ff8ece7a1e2a5d0573f7f4ab3eb2e10aa
Greshch <greshchook@gmail.com>
2022-11-14 18:00:38 +0200

    task 2 was partly solved key = k5fq

--- main.cpp
+++ main.cpp
@@ -11,20 +11,26 @@
 int main() {
     std::string foldersPath = "D:/projects/cplus/Apriorit/second/BruteForce/";
     //std::string namePlainText = foldersPath + "plain_text";
-    std::string nameEncryptedText = foldersPath + "chipher_text";
-    //std::string nameDecryptedText = foldersPath + "decrypted_text";
+    //std::string nameEncryptedText = foldersPath + "chipher_text";
+    std::string nameEncryptedText = foldersPath + "chipher_text_brute_force";
+    std::string nameDecryptedText = foldersPath + "decrypted_text";
     try {
         Md_5Algorithm algo;
         algo.AddToVocab('a', 'z');
+        algo.AddToVocab('0', '9');
         algo.SetMaxLenOfPassword(4);
         auto begin = std::chrono::system_clock::now();
         time_t beginTime = std::chrono::system_clock::to_time_t(begin);
 
-        algo.SearchPassword(nameEncryptedText);
+        bool isFound = algo.SearchPassword(nameEncryptedText);
 
         auto finish = std::chrono::system_clock::now();
         time_t finishTime = std::chrono::system_clock::to_time_t(finish);
         std::cout << "time: " << finishTime - beginTime << std::endl;
+        if (isFound)
+        {
+            algo.Decrypt(nameDecryptedText, nameEncryptedText);
+        }
     }
     catch (const std::runtime_error& ex) {
         std::cerr << ex.what();
